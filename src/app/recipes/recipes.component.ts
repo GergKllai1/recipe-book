5808283555220cabe199417914d9bd29b754016a
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-20 10:32:39 +0100

    Iterates through the recipes in the html using ngFor

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipes/recipe.model'
 
 @Component({
   selector: 'app-recipes',
@@ -8,10 +7,6 @@ import { Recipe } from '../recipes/recipe.model'
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test for the recipe', 
-    'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
-  ];
 
   constructor() { }
 
