f3e731897f57009309404da71e615b312eff2f57
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-23 15:59:58 +0100

    Recipe detail only appear when clicking on single recipe, using multiple outputs and inputs to transfer data between components

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../../recipe.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,11 +8,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeDetail = new EventEmitter<void>()
 
   constructor() { }
   
 
   ngOnInit() {
+  }
+
+  recipeChosen() {
+    this.recipeDetail.emit()
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -13,9 +13,18 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is a test for the recipe', 
     'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
   ];
+  @Output() recipeDetail = new EventEmitter<{name: string, description: string, imagePath: string}>()
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openRecipeDetail(recipe) {
+    this.recipeDetail.emit({
+      name: recipe.name,
+      description: recipe.description,
+      imagePath: recipe.imagePath
+    })
   }
 
 }
