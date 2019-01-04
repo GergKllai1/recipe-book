a84c8f2c5fe3869411b5a046ef134cb4fbd208f3
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-04 08:29:38 +0100

    Creates the adds to shopping list function with injecting services into each other to transfer data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,35 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test for the recipe',
-    // tslint:disable-next-line:max-line-length
-    'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg'),
-    new Recipe('Another Test Recipe', 'This is also just a test recipe, but another one!',
-    'https://amp.businessinsider.com/images/5a7dc169d03072af008b4bf2-750-562.jpg')
+    new Recipe('Crispy chicken with rice and vegtables', 'Difficulty: 1, Estimated time: 30 minutes',
+    'https://saglikliyasamuzmani.com/wp-content/uploads/2018/03/meat-and-chicken-recipes.jpg',
+    [
+    new Ingredient('Rice', 25),
+    new Ingredient('Chicken', 2),
+    new Ingredient('Vegtables', 5),
+  ]),
+    new Recipe('Hamburger with fried onions', 'Difficulty: 1, Estimated time: 25 minutes',
+    'https://amp.businessinsider.com/images/5a7dc169d03072af008b4bf2-750-562.jpg',
+    [
+      new Ingredient('Beef', 1),
+      new Ingredient('Hamburger buns', 2),
+      new Ingredient('Fried onion', 2),
+    ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
